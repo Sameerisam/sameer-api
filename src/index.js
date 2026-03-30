1c96a6ebb0013bc7cb4eb9923fc38fea686783eb
sameerahmad-ai <sameerahmada677@gmail.com>
2026-03-31 02:45:11 +0500

    add user and video model

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import dotenv from "dotenv";
 import connectDB from "./db/db.js";
 
 dotenv.config({
-    path:"./.env"
-})
-connectDB();
+  path: "./.env",
+});
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("error hai server running sai pahly", error);
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("DataBase connection failed!!!!", err);
+  });
